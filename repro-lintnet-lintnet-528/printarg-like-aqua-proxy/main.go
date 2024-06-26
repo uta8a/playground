bfcba2f9ce39366b82e5bcc0fa27ae9f4dfd2684
Masanori Tani <uta8a@uta8a.org>
2024-06-26 23:24:09 +0000

    add reference to code comment

--- repro-lintnet-lintnet-528/printarg-like-aqua-proxy/main.go
+++ repro-lintnet-lintnet-528/printarg-like-aqua-proxy/main.go
@@ -1,3 +1,6 @@
+// reference
+// suzuki-shunsuke repro https://github.com/lintnet/lintnet/issues/528#issuecomment-2192696724
+// aqua-proxy https://github.com/aquaproj/aqua-proxy/blob/9dc658ce3cb3c62203c1819d5828db5733b126c8/pkg/cli/xsys.go#L14-L28
 package main
 
 import (
