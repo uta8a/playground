ce05f1fa441e59c715de3e9d3bf1987972caa310
Masanori Tani <uta8a@uta8a.org>
2024-06-26 21:48:49 +0000

    try1 gh: Could not resolve to a node with the global id of '***'

---
+++ github-projects-graphql/create-custom-settings.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euxo pipefail
+
+gh api graphql -F number=$GITHUB_PROJECT_ID -f query="mutation {
+  createProjectV2Field(input: {
+    projectId: \"$GITHUB_PROJECT_ID\"
+    name: \"$GITHUB_PROJECT_CUSTOM_FIELD_NAME\"
+    dataType: TEXT
+  }) {
+    projectV2Field {
+      ... on ProjectV2Field {
+       id
+      name
+      dataType 
+      }
+    }
+  }
+}"
