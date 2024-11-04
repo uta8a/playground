f2555e2abedc803622d334f169021fe285788945
Masanori Tani <uta8a@uta8a.org>
2024-11-04 20:16:24 +0900

    04 internal module

--- go-with-bazel/apps/hello_world/main.go
+++ go-with-bazel/apps/hello_world/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/uta8a/playground/go-with-bazel/internal/reverse"
 )
 
 func main() {
 	uuidStr := uuid.NewString()
 	fmt.Printf("Hello, World!(%s)\n", uuidStr)
+	fmt.Printf("Reversed: %s\n", reverse.String("Hello, World!"))
 }
