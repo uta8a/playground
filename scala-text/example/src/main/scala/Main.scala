d032ae2273cc9324b0d34d2e70ad84b3208795ec
Masanori Tani <uta8a@uta8a.org>
2025-09-13 06:49:26 +0900

    class 練習問題やった

--- scala-text/example/src/main/scala/Main.scala
+++ scala-text/example/src/main/scala/Main.scala
@@ -1,32 +1,7 @@
-def firstLastSame[T](l: List[T]): Boolean = {
-  l.head == l.last
-}
+class Point3D(val x: Int, val y: Int, val z: Int)
+@main def main(): Unit =
+  val p = new Point3D(1,2,3)
+  println(p.x)
+  println(p.y)
+  println(p.z)
 
-def printfirstLastSame1000Times(): Unit = {
-  for (i <- 1 to 1000) {
-    val head = new scala.util.Random(new java.security.SecureRandom()).alphanumeric.take(5).toList
-    head match {
-      case List(a,b,c,d,e) => println(s"$a$b$c$d$a")
-    }
-  }
-}
-var age: Int = 5
-var isSchoolStarted: Boolean = false
-
-def isChild(age: Int, isSchoolStarted: Boolean): Unit = {
-  if ((1 <= age && age <= 6) && !isSchoolStarted) {
-    println("幼児です")
-  } else {
-    println("幼児ではありません")
-  }
-}
-
-class User(val name: String, val age: Int)
-
-object User {
-  def printUser(user: User) = println(user.name + " " + user.age)
-}
-
-@main def hello(): Unit =
-  isChild(age, isSchoolStarted)
-  printfirstLastSame1000Times()
