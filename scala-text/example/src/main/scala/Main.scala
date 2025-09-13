dd8d10d2ddc91292989355440c6152d0e5cd43a6
Masanori Tani <uta8a@uta8a.org>
2025-09-13 17:08:57 +0900

    collection: array swap

--- scala-text/example/src/main/scala/Main.scala
+++ scala-text/example/src/main/scala/Main.scala
@@ -1,12 +1,8 @@
-trait Logger(prefix: String):
-  // 本体が「トレイトの初期化コード」= コンストラクタに相当
-  def log(msg: String): Unit =
-    println(s"$prefix$msg")
-
-class Service extends Logger("SVC: "):
-  def work(): Unit = log("doing work")
-
-@main def demo(): Unit =
-  new Service().work()
-// => SVC: doing work
-
+def swapArray[T] (arr: Array[T])(i: Int, j: Int): Unit =
+  val tmp = arr(i)
+  arr(i) = arr(j)
+  arr(j) = tmp
+@main def run(): Unit =
+  val arr = Array(1,2,3,4,5)
+  swapArray(arr)(0,1)
+  println(arr.foreach(println))
