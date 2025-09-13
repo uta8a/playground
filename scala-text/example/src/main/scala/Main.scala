a0850c9a7fa378e06896fdae2e8963b23d201659
Masanori Tani <uta8a@uta8a.org>
2025-09-13 16:32:18 +0900

    匿名クラスを作ってtraitをインスタンス化する

--- scala-text/example/src/main/scala/Main.scala
+++ scala-text/example/src/main/scala/Main.scala
@@ -1,19 +1,9 @@
-trait A:
-  def who = "A"
+trait Greeter:
+  def greet(name: String): String
 
-trait B:
-  def who = "B"
-
-trait C:
-  def who = "C"
-
-class X extends A, B, C: // 左→右の順で線形化（後勝ち）
-  override def who =
-    // 必要なら特定のトレイト経由で super 呼び分け
-    val fromB = super[B].who
-    val fromC = super[C].who
-    s"X(${fromB}|${fromC})"  // => "X(B|C)"
+val g = new Greeter:
+  def greet(name: String) = s"Hello, $name"
 
 @main def run(): Unit =
-  println(new X().who)
+  println(g.greet("world"))
 
