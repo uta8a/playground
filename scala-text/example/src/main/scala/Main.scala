ca1c1996aa2596a805a277b53b1cdad694dcc456
Masanori Tani <uta8a@uta8a.org>
2025-09-13 16:30:10 +0900

    複数のトレイトをmixinする

--- scala-text/example/src/main/scala/Main.scala
+++ scala-text/example/src/main/scala/Main.scala
@@ -1,7 +1,19 @@
-class Point3D(val x: Int, val y: Int, val z: Int)
-@main def main(): Unit =
-  val p = new Point3D(1,2,3)
-  println(p.x)
-  println(p.y)
-  println(p.z)
+trait A:
+  def who = "A"
+
+trait B:
+  def who = "B"
+
+trait C:
+  def who = "C"
+
+class X extends A, B, C: // 左→右の順で線形化（後勝ち）
+  override def who =
+    // 必要なら特定のトレイト経由で super 呼び分け
+    val fromB = super[B].who
+    val fromC = super[C].who
+    s"X(${fromB}|${fromC})"  // => "X(B|C)"
+
+@main def run(): Unit =
+  println(new X().who)
 
